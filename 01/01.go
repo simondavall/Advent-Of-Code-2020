872bb496a715a89748bf7a6cf50b8bead876ea6e
Simon Da Vall <simon.davall@gmail.com>
2026-01-11 16:26:36 +0000

    Revisited Day01

--- 01/01.go
+++ 01/01.go
@@ -13,33 +13,6 @@ var url string = "https://adventofcode.com/2020/day/1"
 var expectedResult1 int64 = 605364
 var expectedResult2 int64 = 128397680
 
-func main() {
-	var resultPartOne int64 = -1
-	var resultPartTwo int64 = -1
-
-	fmt.Printf("\n%s", title)
-	fmt.Printf("\n%s\n", url)
-	for i := 1; i < len(os.Args); i++ {
-		filePath := os.Args[i]
-		fmt.Printf("\nFile: %s\n", filePath)
-
-		input, err := os.ReadFile(filePath)
-		check(err)
-
-		startPart1 := time.Now()
-		resultPartOne = PartOne(input)
-		fmt.Printf("Part 1 result: %d in %s\n", resultPartOne, time.Since(startPart1))
-		
-		startPart2 := time.Now()
-		resultPartTwo = PartTwo(input)
-		fmt.Printf("Part 2 result: %d in %s\n", resultPartTwo, time.Since(startPart2))
-	}
-	if resultPartOne != expectedResult1 || resultPartTwo != expectedResult2 {
-		fmt.Println("Incorrect result")
-		os.Exit(1);
-	}
-}
-
 func PartOne(input []byte) int64 {
 	expenses := ProcessInput(input)
 	for i := 0; i < len(expenses)-1; i++ {
@@ -81,6 +54,33 @@ func ProcessInput(input []byte) []int {
 	}
 
 	return ints
+}
+
+func main() {
+	var resultPartOne int64 = -1
+	var resultPartTwo int64 = -1
+
+	fmt.Printf("\n%s", title)
+	fmt.Printf("\n%s\n", url)
+	for i := 1; i < len(os.Args); i++ {
+		filePath := os.Args[i]
+		fmt.Printf("\nFile: %s\n", filePath)
+
+		input, err := os.ReadFile(filePath)
+		check(err)
+
+		startPart1 := time.Now()
+		resultPartOne = PartOne(input)
+		fmt.Printf("Part 1 result: %d in %s\n", resultPartOne, time.Since(startPart1))
+		
+		startPart2 := time.Now()
+		resultPartTwo = PartTwo(input)
+		fmt.Printf("Part 2 result: %d in %s\n", resultPartTwo, time.Since(startPart2))
+	}
+	if resultPartOne != expectedResult1 || resultPartTwo != expectedResult2 {
+		fmt.Println("Incorrect result")
+		os.Exit(1);
+	}
 }
 
 func check(e error) {
